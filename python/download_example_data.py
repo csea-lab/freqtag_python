9b64881bbd6d0aee7c0260c05b42f0de09da73bf
veliebm <veliebm@gmail.com>
2022-03-08 14:07:28 -0500

    Check if path exists before downloading

--- python/download_example_data.py
+++ python/download_example_data.py
@@ -2,11 +2,24 @@
 Downloads the example data used for the freqtag pipeline.
 """
 import urllib.request
+from pathlib import Path
+
+FILES_TO_DOWNLOAD = {
+    "https://osf.io/pmge9/download": "raw/exampledata_1.mat",
+    "https://osf.io/hb7sd/download": "raw/exampledata_2.mat",
+}
 
 
 def download_data():
-    urllib.request.urlretrieve("https://osf.io/pmge9/download", "raw/exampledata_1.mat")
-    urllib.request.urlretrieve("https://osf.io/hb7sd/download", "raw/exampledata_2.mat")
+    """
+    Downloads data listed in FILES_TO_DOWNLOAD if they aren't already downloaded.
+    """
+    for from_url, to_path in FILES_TO_DOWNLOAD.items():
+        to_path = Path(to_path)
+        if not to_path.exists():
+            print(f"Downloading {to_path} from {from_url}")
+            to_path.parent.mkdir(exist_ok=True, parents=True)
+            urllib.request.urlretrieve(from_url, to_path)
 
 
 if __name__ == "__main__":
