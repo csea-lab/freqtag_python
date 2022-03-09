6f96cbbab2c32f4287179f8c56e812f56524f20a
veliebm <veliebm@gmail.com>
2022-03-09 08:50:48 -0500

    Make line length PEP8

--- python/freqtag_pipeline_example1.py
+++ python/freqtag_pipeline_example1.py
@@ -1,5 +1,7 @@
 """
-The freqtag pipeline: A principled approach to analyzing electrophysiological time series in frequency tagging paradigms. Part 1: Measuring the time-varying ssVEP envelope at each tagging frequency.
+The freqtag pipeline: A principled approach to analyzing electrophysiological
+time series in frequency tagging paradigms. Part 1: Measuring the time-varying
+ssVEP envelope at each tagging frequency.
 """
 import scipy.io
 
@@ -15,7 +17,8 @@ def load_data():
     """
     1-Load the 3-D file (129 electrodes x 3901 time points x 39 trials)
     - Each trial contains  400ms pre- and 7400ms post-stimulus onset;
-    - During each trial, two spatially overlapping stimuli flicker at different frequencies (5 Hz and 6 Hz),
+    - During each trial, two spatially overlapping stimuli flicker at different
+    frequencies (5 Hz and 6 Hz),
     """
     download_examples.download()
     return scipy.io.loadmat("raw/exampledata_1.mat")["data_1"]
