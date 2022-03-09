8595ecd360858188af7816af2109f2c975a59904
veliebm <veliebm@gmail.com>
2022-03-09 09:36:53 -0500

    Add step to get faxis and prep spectral analysis

--- python/freqtag_pipeline_example1.py
+++ python/freqtag_pipeline_example1.py
@@ -3,25 +3,22 @@ The freqtag pipeline: A principled approach to analyzing electrophysiological
 time series in frequency tagging paradigms. Part 1: Measuring the time-varying
 ssVEP envelope at each tagging frequency.
 """
+import numpy
 import scipy.io
-
 import download_examples
 
 
 def main():
-    exampledata_1 = load_data()
-    breakpoint()
-
-
-def load_data():
-    """
-    1-Load the 3-D file (129 electrodes x 3901 time points x 39 trials)
-    - Each trial contains  400ms pre- and 7400ms post-stimulus onset;
-    - During each trial, two spatially overlapping stimuli flicker at different
-    frequencies (5 Hz and 6 Hz),
-    """
+    # 1-Load the 3-D file (129 electrodes x 3901 time points x 39 trials)
     download_examples.download()
-    return scipy.io.loadmat("raw/exampledata_1.mat")["data_1"]
+    exampledata_1 = scipy.io.loadmat("raw/exampledata_1.mat")["data_1"]
+
+    # 3-Define frequency axis and prepare spectral analysis, see section 3.1
+    data_ssvep = exampledata_1[:, 700:3700, :]
+    faxisall = numpy.arange(0, 250, 0.1667)
+    faxis = faxisall[:196]
+
+    breakpoint()
 
 
 if __name__ == "__main__":
