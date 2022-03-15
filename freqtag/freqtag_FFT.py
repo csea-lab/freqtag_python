658063f17b1ba02c2198e7e927f48f91e3306434
veliebm <veliebm@gmail.com>
2022-03-15 09:24:25 -0400

    Make compatible with freqtag pipeline

--- freqtag/freqtag_FFT.py
+++ freqtag/freqtag_FFT.py
@@ -1,41 +1,39 @@
-import numpy
+import numpy as np
+from typing import List
 
 
-def FFT(data: numpy.array, fsamp: float) -> numpy.array:
+def FFT(data: np.array, fsamp: float) -> List[np.array]:
     """
     Apply the Discrete Fourier Transform on a 2-D array.
 
     Args:
-        data (numpy.array): Sensors by time points 2-D matrix
+        data (np.array): Sensors by time points 2-D matrix
         fsamp (float): Sampling rate in Hz.
 
     Returns:
-        numpy.array: Columns in the array are:
+        np.array: Columns in the array are:
             amp: amplitude spectrum (amp)
             phase: phase spectrum
             freqs: frequencies available in the spectrum
             fftcomp: complex fourier components
     """
-    # NFFT = data.shape[1]
-    # numpy.fft.fft(a, n=None, axis=- 1, norm=None)
-    # fftcomp = fft(data, NFFT)
-	# phase = angle(fftcomp);      % Calculate the phase
-    # Mag = abs(fftcomp);          % Calculate the amplitude
-               
-	
-	# Mag(1,:) = Mag(1,:)/2;                                             % DC Frequency not twice
-	# if ~rem(NFFT,2)                                                    % Nyquist Frequency not twice
-	# 		Mag(NFFT/2+1, :)=Mag(NFFT/2+1, :)./2;
-	# end
-	
-	# Mag=Mag/NFFT;               % After computing the fft, the coefficients will be 
-    #                             % scaled in terms of frequency (in Hz) 
-    
-    # Mag = Mag';                 % Sensors as rows again
-    # phase = phase';
-    
-    # amp = Mag(:,1:round(NFFT./2));              % Scaling the power
-    # phase  = phase(:,1:round(NFFT./2));         % Scaling the phase
-    # select = 1:(NFFT+1)/2;                      % Scaling the frequencies
-    # freqs = (select - 1)'*fsamp/NFFT;
-    pass
+    num_points = data.shape[-1]
+    midpoint = round(num_points / 2)
+    untrimmed_freqs = np.fft.fftfreq(num_points, d=1/fsamp)
+
+    fftcomp = np.fft.fftn(data, axes=[-1])
+    untrimmed_phase = np.angle(fftcomp)
+
+    # Get amplitude, taking care of doubled DC or Nyquist frequencies.
+    untrimmed_amp = np.abs(fftcomp)
+    untrimmed_amp[:, 0] = untrimmed_amp[:, 0] / 2
+    if num_points % 2 == 0:
+        untrimmed_amp[:, midpoint] = untrimmed_amp[:, midpoint] / 2
+    untrimmed_amp = untrimmed_amp / num_points
+
+    # Trim to remove opposite side of FFT operation.
+    phase = untrimmed_phase[:, :midpoint]
+    freqs = untrimmed_freqs[:midpoint]
+    amp = untrimmed_amp[:, :midpoint]
+
+    return [amp, phase, freqs, fftcomp]
