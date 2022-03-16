ec393609b93f490d0b39ba1402bf7a0f05c44d66
veliebm <veliebm@gmail.com>
2022-03-16 09:32:03 -0400

    Abbreviate numpy as np

--- freqtag/freqtag_pipeline_example1.py
+++ freqtag/freqtag_pipeline_example1.py
@@ -3,7 +3,7 @@ The freqtag pipeline: A principled approach to analyzing electrophysiological
 time series in frequency tagging paradigms. Part 1: Measuring the time-varying
 ssVEP envelope at each tagging frequency.
 """
-import numpy
+import numpy as np
 import scipy.io
 import download_examples
 from freqtag_FFT import FFT
@@ -18,11 +18,11 @@ def main():
 
     # 3-Define frequency axis and prepare spectral analysis, see section 3.1
     data_ssvep = exampledata_1[:, 700:3700, :]
-    faxisall = numpy.arange(0, 250, 0.1667)
+    faxisall = np.arange(0, 250, 0.1667)
     faxis = faxisall[:196]
 
     # 4-Run a Discrete Fourier Transform on the data
-    mean_ssvep = numpy.mean(data_ssvep, 2)
+    mean_ssvep = np.mean(data_ssvep, 2)
     amp, phase, freqs, fftcomp = FFT(mean_ssvep, SAMPLE_RATE)
 
 
