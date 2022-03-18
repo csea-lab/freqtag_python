576c62cb4d45d8cd4cd5b806c245f890110fa78b
veliebm <veliebm@gmail.com>
2022-03-18 09:22:41 -0400

    Fix type hints

--- freqtag/freqtag_FFT.py
+++ freqtag/freqtag_FFT.py
@@ -2,7 +2,7 @@ import numpy as np
 from typing import List
 
 
-def freqtag_FFT(data: np.array, fsamp: float) -> List[np.array]:
+def freqtag_FFT(data: np.ndarray, fsamp: float | int) -> List[np.ndarray]:
     """
     Applies the Discrete Fourier Transform on EEG data.
 
