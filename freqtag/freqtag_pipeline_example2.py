92b1074a8f34d8ff3f6d863c027852800326cc0a
veliebm <veliebm@gmail.com>
2022-03-25 10:48:36 -0400

    Add freqtag_FFT step (step 2)

--- freqtag/freqtag_pipeline_example2.py
+++ freqtag/freqtag_pipeline_example2.py
@@ -6,6 +6,8 @@ Part 2: Measuring the ssVEP mplitude from single trials, using sliding window av
 """
 import download_examples
 import scipy.io
+import numpy as np
+from freqtag_FFT import freqtag_FFT
 
 
 SAMPLE_RATE = 500
@@ -15,6 +17,11 @@ def main():
     # 1-Load the 3-D file (109 electrodes x 2500 time points x 15 trials)
     download_examples.download()
     exampledata_2 = scipy.io.loadmat("raw/exampledata_2.mat")["data_2"]
+
+    # 2-Data visualization and check in the frequency domain
+    mean_exampledata = np.mean(exampledata_2, 2)
+    amp, phase, freqs, fftcomp = freqtag_FFT(mean_exampledata, SAMPLE_RATE)
+    # TODO: Add plots for this section.
 
 
 if __name__ == "__main__":
