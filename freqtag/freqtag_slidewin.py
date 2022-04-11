60de6ff83e64048a71b08420648be9b503a94cb6
veliebm <veliebm@gmail.com>
2022-04-11 12:08:54 -0400

    Make function work

--- freqtag/freqtag_slidewin.py
+++ freqtag/freqtag_slidewin.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 import scipy.signal
 
@@ -39,8 +40,9 @@ def freqtag_slidewin(
       List containing arrays in the following order:
         (m sensors, k trials) array.
           ssVEP amplitude at the frequency of interest for each trial.
-        (m sensors, TODO averages, k trials) array.
+        (m sensors, averages, k trials) array.
           Sliding window averages for each trial in the time domain.
+          Number of averages is equal to the number of shiftcycles.
         (m sensors, k trials) array.
           Phase stability average of complex coefficients across moving windows.
         (m sensors, k trials) array.
@@ -48,7 +50,10 @@ def freqtag_slidewin(
           for each trial.
     """
     # TODO: Input validation.
+    # TODO: Modularize this function more.
 
+    NTrials = data.shape[-1]
+    NSensors = data.shape[0]
     sampcycle = 1000 / sampnew
     tempvec = round((1000 / foi) / sampcycle)
     longvec = np.full(200, tempvec)
@@ -66,18 +71,19 @@ def freqtag_slidewin(
     min_diff_vec = np.abs(freqbins - foi)
     targetbin = np.flatnonzero(min_diff_vec == np.min(min_diff_vec))
 
-    trialamp = []
-    trialSNR = []
-    phasestabmat = []
+    trialamp = np.empty([NSensors, NTrials])
+    trialSNR = np.empty([NSensors, NTrials])
+    phasestabmat = np.empty([NSensors, NTrials])
 
-    NTrials = data.shape[-1]
+    winmat3d = np.empty([NSensors, shiftcycle, NTrials])
 
     print("Trial index:")
 
+    # TODO: Speed this loop up.
     for trial in range(NTrials):
 
         Data = data[:, ssvepvec, trial]
-        fouriersum = []
+        fouriersum = None
         print(trial)
 
         # TODO: Check that resampling works on various datasets.
@@ -95,10 +101,60 @@ def freqtag_slidewin(
             win_end = win_start + shiftcycle
             index = np.arange(win_start, win_end)
             regression_input = datamat[:, tuple(index)]
-            winmatsum += freqtag_regressionMAT(
-                regression_input
-            )
+            regressed_mat = freqtag_regressionMAT(regression_input)
+            winmatsum += regressed_mat
 
-            pass
+            fouriermat = np.fft.fft(regressed_mat, axis=1)
 
-    pass
+            fouriercomp = fouriermat[:, targetbin].squeeze()
+            if fouriersum is None:
+                fouriersum = fouriercomp / np.abs(fouriercomp)
+            else:
+                fouriersum += fouriercomp / np.abs(fouriercomp)
+
+            # TODO: Add plots.
+
+        winmat = winmatsum / len(winshiftvec)
+        NFFT = shiftcycle - 1
+        NumUniquePoints = math.ceil(shiftcycle / 2)
+        fftMat = np.fft.fft(winmat, n=NFFT, axis=1)
+        Mag = _get_corrected_magnitude(fftMat)
+
+        trialamp[:, trial] = Mag.T[targetbin]
+        trialSNR[:, trial] = _get_trialSNR(Mag, targetbin)
+
+        phasestabmat[:, trial] = np.abs(fouriersum / (winshiftstep + 1))
+        winmat3d[:, :, trial] = winmat
+
+    return trialamp, winmat3d, phasestabmat, trialSNR
+
+
+def _get_trialSNR(Mag: np.ndarray, targetbin: int) -> np.ndarray:
+    """
+    Returns a very crude SNR of a trial within the magnitude array.
+    """
+    trial = Mag.T[targetbin].squeeze()
+    older_trial = Mag.T[targetbin - 2].squeeze()
+    newer_trial = Mag.T[targetbin + 2].squeeze()
+    older_and_newer_trials = np.stack([older_trial, newer_trial])
+    mean_of_older_and_newer = np.mean(older_and_newer_trials, axis=0)
+    snr = np.log10(trial / mean_of_older_and_newer) * 10
+
+    return snr
+
+
+def _get_corrected_magnitude(fftMat: np.ndarray) -> np.ndarray:
+    """
+    Returns the adjusted magnitudes of a complex FFT matrix.
+
+    Corrects magnitudes by multiplying them by 2 and dividing by the total number
+    of points. NOTE: Unlike in the MATLAB code, doesn't do anything more for
+    the Nyquist or DC frequencies.
+    """
+    num_points = fftMat.shape[1]
+
+    Mag = np.abs(fftMat)
+    Mag = Mag * 2
+    Mag = Mag / num_points
+
+    return Mag
